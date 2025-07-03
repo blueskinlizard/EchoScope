2b3fd05541d741fa9b81630438b24a5812d80971
blueskinlizard <blueskinlizard@gmail.com>
2025-07-03 09:54:24 -0500

    Backend scaffolding & structure created

---
+++ src/server/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require("cors");
+const http = require("http"); 
+
+const file_routes = require('./fileRoutes')
+const server = http.createServer(app);
+
+
+const app = express();
+const PORT = 8080;
+
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+};
+app.use(cors(corsOptions));
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/', file_routes);
+
+server.listen(PORT, () =>{
+    console.log(`App running up on port: ${PORT}`)
+})
